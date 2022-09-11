611c327a0e1f1de21e101b68ca3c2572f792f092
demonkillerr <gaurangvishwakarma619@gmail.com>
2022-09-12 03:12:41 +0530

    remove typo

--- etc/calamares/launch.sh
+++ etc/calamares/launch.sh
@@ -1,7 +1,7 @@
-**#!/usr/bin/bash
+#!/usr/bin/bash
 
-#DIR="/etc/calamares"
-#KERNEL=`uname -r`
+DIR="/etc/calamares"
+KERNEL=`uname -r`
 
 #if [[ -d "/run/archiso/copytoram" ]]; then
 #	sudo sed -i -e 's|/run/archiso/bootmnt/arch/x86_64/airootfs.sfs|/run/archiso/copytoram/airootfs.sfs|g' "$DIR"/modules/unpackfs.conf
